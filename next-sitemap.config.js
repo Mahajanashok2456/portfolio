6d0670e6cc9ff35fa0c26ba35212f093e7a3b9ca
Mahajanashok2456 <mahajanashok78@gmail.com>
2026-04-05 01:05:52 +0530

    fix: manual sitemap generation and config update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,23 @@
 /** @type {import('next-sitemap').IConfig} */ 
  module.exports = { 
    siteUrl: 'https://mahajanashok.in', 
-   generateRobotsTxt: false, 
+   generateRobotsTxt: true, 
+   sitemapSize: 7000,
    changefreq: 'weekly', 
    priority: 0.8, 
    exclude: ['/api/*'], 
+   robotsTxtOptions: {
+     policies: [
+       {
+         userAgent: '*',
+         allow: '/',
+         disallow: '/api/',
+       },
+     ],
+     additionalSitemaps: [
+       'https://mahajanashok.in/sitemap.xml',
+     ],
+   },
    additionalPaths: async (config) => [ 
      { loc: '/', changefreq: 'daily', priority: 1.0, lastmod: new Date().toISOString() }, 
      { loc: '/#projects', changefreq: 'weekly', priority: 0.9 }, 
